f3b4e20a6b892ba0f38b74a69379c37f5c4e88de
sifa <sifa17@qq.com>
2023-09-18 19:41:05 +0800

    tab of linux!

--- 07_convergence/csubst_analysis.py
+++ 07_convergence/csubst_analysis.py
@@ -12,21 +12,25 @@ def batch_run_csubst(aln_dir, tree_dir, fg_path, cwd_dir, og_list, n_jobs=2):
         delayed(run_csubst)(
             os.path.join(aln_dir, og) + '.fa',  # OG0000000.fa
             os.path.join(tree_dir, og) + '_tree.txt', # OG0000000_tree.txt
-            os.path.abspath(fg_path),
+            fg_path,
             os.path.join(cwd_dir, og) # OG0000000/
         )
         for og in og_list
     )
 
 if __name__ == '__main__':
-    fg_path = '/public1/user/sifa/projects/vivipary/07_convergent_analy/foreground.txt' # foreground.txt path
+    fg_path = '/public1/user/sifa/projects/vivipary/CODE/07_convergence/foreground.tsv' # foreground.txt path
 
     aln_dir = '/public1/user/sifa/projects/vivipary/06_downstream/trimmed_aligned_cds' # trimmed aligned cds
     tree_dir = '/public1/user/sifa/projects/vivipary/01_ortho_results/Resolved_Gene_Trees' # rooted_tree
 
-    og_list = [og[:-3] for og in os.listdir(aln_dir)]
-
     cwd = '/public1/user/sifa/projects/vivipary/07_convergent_analy'
+
+    fine_trimmed = '/public1/user/sifa/projects/vivipary/fine_trimmed_ogs.txt'
+    og_list = []
+    with open(fine_trimmed, 'r') as f:
+        for line in f:
+            og_list.append(line.strip()[:-3])
 
     for og in og_list:
         if not os.path.exists(os.path.join(cwd, og)):
